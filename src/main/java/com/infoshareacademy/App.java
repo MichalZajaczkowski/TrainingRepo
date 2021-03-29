393abe83a20b5d53c8afe72062f706de080d8786
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-29 20:42:19 +0200

    task4, change from public to private fields, add getter and setter

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,13 +1,39 @@
 package com.infoshareacademy;
 
+import com.infoshareacademy.vehicle.Car;
+import com.infoshareacademy.vehicle.Engine;
+
 public class App {
 
     public static void main(String[] args) {
+// ex4
+        Car car1 = new Car();
+        Car car2 = new Car();
 
-// ex3
-        StaticExample staticExample = new StaticExample();
-        staticExample.nonStaticMethod();
+        Engine engine1 = new Engine();
+        Engine engine2 = new Engine();
 
-        StaticExample.staticMethod();
+        engine1.setPower(150);
+        engine1.setCapacity(1500);
+        engine2.setPower(254);
+        engine2.setCapacity(2462);
+
+        car1.setName("car1");
+        car1.setEngine(engine1);
+        car1.setMaxSpeed(155);
+
+        car1.printName();
+        car1.getEngine().printPower();
+        car1.getEngine().printCapacity();
+        car1.printMaxSpeed();
+        System.out.println("+++++++++++++++");
+        car2.setName(car2.getName());
+        car2.setEngine(engine2);
+        car2.setMaxSpeed(255);
+
+        car2.printName();
+        car2.getEngine().printPower();
+        car2.getEngine().printCapacity();
+        car2.printMaxSpeed();
     }
 }
---
+++ src/main/java/com/infoshareacademy/vehicle/Car.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.vehicle;
+
+public class Car {
+    private String name = "defoult";
+    private Engine engine;
+    private int maxSpeed;
+
+
+    public Car() {
+
+    }
+    public Car(String name){
+        this.name = name;
+        maxSpeed = 150;
+    }
+    public void printName() {
+        System.out.println(name);
+    }
+    public void printMaxSpeed() {
+        System.out.println(maxSpeed);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
