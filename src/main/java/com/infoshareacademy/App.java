cd3f8a40da382fd44eb590070161a4d3a9576861
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-30 18:55:56 +0200

    task15, pobieranie danych z klawiatury i przypisanie ich

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -3,41 +3,25 @@ package com.infoshareacademy;
 import com.infoshareacademy.vehicle.Car;
 import com.infoshareacademy.vehicle.Engine;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nazwa samochodu");
+        String name = scanner.nextLine();
+        System.out.println("moc silnika");
+        Integer enginePower = scanner.nextInt();
 
-        System.out.println("++++++++++while++++++++++"); // kiedy nie znamy ilości obiegów pętli
-        Integer i = 5;
-        while (i > 0) {
-            System.out.println(i);
-            i--;
-        }
-        System.out.println("++++++++++doWhile++++++++++");
-        Integer q = 5;
-        do {
-            q--;
-            System.out.println(q);
-        } while (q > 0);
-        System.out.println("++++++++++for++++++++++");// kiedy  znamy ilość obiegów pętli
-        Integer ln = 4;
-        for (int j = 0; j < ln; j++) {
-            System.out.println(j);
-        }
+        Engine engine = new Engine();
+        engine.setPower(enginePower);
+        Car car = new Car();
+        car.setName(name);
+        car.setEngine(new Engine());
+        car.setEngine(engine);
 
-        System.out.println("++++++++++continue / break++++++++++");// kiedy  znamy ilość obiegów pętli
-        number11(15);
-    }
-
-    public static void number11(int a) {
-        for (int j = 0; j < a; j++) {
-            if (j % 2 == 0) {
-                continue;
-            }
-            if (j % 11 == 0) {
-                break;
-            }
-            System.out.println(j);
-        }
+        car.printName();
+        car.getEngine().printPower();
     }
 }
