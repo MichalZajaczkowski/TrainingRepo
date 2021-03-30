c64873c8096503869989cc08dca0c30e44cdf12a
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-03-30 17:53:32 +0200

    task8, upgrade

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -10,15 +10,21 @@ public class App {
         Car car1 = new Car("car1");
         car1.setMaxSpeed(110);
         Car car2 = new Car("car2");
-        car2.setMaxSpeed(250);
+        car2.setMaxSpeed(110);
 
+        getNameOfFasterCar(car1, car2);
+    }
+
+    private static void getNameOfFasterCar(Car car1, Car car2) {
         if (car1.getMaxSpeed() > car2.getMaxSpeed()) {
-            System.out.println(car1.getName());
+            car1.printName();
             car1.setName("faster car");
 
         } else if (car2.getMaxSpeed() > car1.getMaxSpeed()) {
-            System.out.println(car2.getName());
+            car2.printName();
             car2.setName("faster car ");
+        } else {
+            System.out.println("prędkości takie same");
         }
         System.out.println("car1: " + car1.getName() + ", car2: " + car2.getName());
     }
