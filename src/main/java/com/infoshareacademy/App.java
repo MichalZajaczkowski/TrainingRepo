e5f8341e5f4dbe1f2628c2117759f93ab17e4331
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-08 21:36:02 +0200

    ex22. try/catch z pętlą/upDate scaner

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-
+        Scanner scanner;
 
         boolean flag = true;
         while (flag) {
             try {
-                Scanner scanner = new Scanner(System.in);
+                scanner = new Scanner(System.in);
                 int i = scanner.nextInt();
                 System.out.println("podano: " + i);
                 flag = false;
