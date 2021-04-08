8fdbda3cbc95d0b6f656f0248968beefc7686b4e
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-08 22:35:15 +0200

    petla foreach i parametry w metodach

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,14 +1,30 @@
 package com.infoshareacademy;
-
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class App {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String nextLine = scanner.nextLine();
-        Integer number = Integer.valueOf(nextLine);
-        System.out.println(number * 3);
+    public static void main(String[] args){
+
+        printNames("asd", "34");
+        System.out.println("");
+        printNames("asd", "asad asd", "we", "235dqw");
+
+        System.out.println("++++++++++++");
+
+        printNames2("123", "asd1");
+        System.out.println("");
+        printNames2("1dxg3","123", "asd1", "adas");
+    }
+
+    private static void printNames(String... strings){
+
+        for (int i = 0; i < strings.length; i++) {
+            String q = strings[i];
+            System.out.println(q);
+        }
+
+    }
+    private static void printNames2(String... strings2) {
+        for (String s : strings2) {
+            System.out.println(s);
+        }
     }
 }
