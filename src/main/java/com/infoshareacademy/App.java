5e16d9dd36ac10493962f1bbcc5d829b729108da
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-08 21:24:40 +0200

    ex22. try/catch z pętlą

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -6,14 +6,19 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        try {
-            System.out.println("Podaj liczbę zmiennoprzecinkową");
-            double scannerDouble = scanner.nextDouble();
-            System.out.println(scannerDouble);
-        } catch (InputMismatchException e){
-            System.out.println("Podałeś błędna wartość ");
+
+        boolean flag = true;
+        while (flag) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int i = scanner.nextInt();
+                System.out.println("podano: " + i);
+                flag = false;
+                //break;
+            }catch (InputMismatchException e) {
+                System.out.println("podałeś błędne dane");
+            }
         }
     }
 }
