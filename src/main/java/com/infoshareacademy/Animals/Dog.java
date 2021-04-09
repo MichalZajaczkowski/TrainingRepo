47c0909704c3774a7f377d3a4f14838862f2ebd5
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-09 22:20:49 +0200

    ex23 - dziedziczenie/super w metodzie

--- src/main/java/com/infoshareacademy/Animals/Dog.java
+++ src/main/java/com/infoshareacademy/Animals/Dog.java
@@ -3,6 +3,7 @@ package com.infoshareacademy.Animals;
 public class Dog extends Animal{
     @Override
     public void eat() {
+        super.eat();
         System.out.println("Dog eat food");
     }
 }
