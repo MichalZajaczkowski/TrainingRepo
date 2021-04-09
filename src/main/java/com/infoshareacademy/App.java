fd9ec2428ff6f6c6cb669654836f8b017ad81b94
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-09 23:48:03 +0200

    interface logika + generowanie losowej liczby/update2

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -9,13 +9,19 @@ import com.infoshareacademy.vehicle.Car;
 import com.infoshareacademy.vehicle.Vehicle;
 
 public class App {
+
+    private static void testInterface(MyInterface myInterface) {
+        myInterface.printMessage();
+    }
     public static void main(String[] args) {
 
         MyInterface myInterface = new MyInterfaceImpl();
         MyInterfaceImpl myInterface1 = new MyInterfaceImpl();
-
         AnotherMyInterfaceImpl anotherMyInterface = new AnotherMyInterfaceImpl();
 
+        testInterface(myInterface);
+        testInterface(anotherMyInterface);
+        
         System.out.println(myInterface.generateNumber(15));
         myInterface1.printMessage();
 
