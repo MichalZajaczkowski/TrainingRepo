4060394c95e69250fac499ea96487b954e4e3f21
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-11 10:21:14 +0200

    klasy abstrakcyjne-wstęp

---
+++ src/main/java/com/infoshareacademy/abstraction/AbstractClass.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.abstraction;
+
+public abstract class AbstractClass {
+    public int number = 2;
+
+    public abstract void  printType();
+
+    public void printNumber(){
+        System.out.println(number);
+    }
+}
---
+++ src/main/java/com/infoshareacademy/abstraction/AbstractClassImpl.java
@@ -0,0 +1,10 @@
+package com.infoshareacademy.abstraction;
+
+public class AbstractClassImpl extends AbstractClass{
+
+    @Override
+    public void printType() {
+        number = 6;
+        System.out.println(this.getClass().getSimpleName());
+    }
+}
