d67ccf8e0d1e1e08338fca4582e19ca502b81c60
MichalZajaczkowski <mzajaczkowski90@gmail.com>
2021-04-12 20:24:29 +0200

    przykład listy jako wartość w mapie

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -1,17 +1,35 @@
 package com.infoshareacademy;
 
-import com.infoshareacademy.vehicle.Car;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class App {
-    public static void main(String[] args) {
-        MyGenericType t1 = new MyGenericType("string");
-        MyGenericType t2 = new MyGenericType(new Car());
 
-        t1.printItemType();
-        t2.printItemType();
-
-        if (t2.getItem() instanceof Car) {
-            ((Car)t2.getItem()).setMaxSpeed(120);
+    public static Map<Integer, List<String>> groupNames(String... names) {
+        //adam, jan, piotr, kasia
+        //3 jan, 4 adam, 5 piotr,kasia
+        Map<Integer, List<String>> result = new HashMap<>();
+        for (String name : names) {
+            // pobranie klucza - czyl długosci imienia
+            int key = name.length();//key
+            //dodanie pustej listy jeśli pod danym kluczem nic nie ma
+            result.putIfAbsent(key, new ArrayList<>());
+            // dodanie listy z danego klucza - zawsze lista istnieje dzieki powyższej instrukcji
+            List<String> value = result.get(key);
+            // dodanie wartości do listy
+            value.add(name);
+            //nadpisanie wartosci pod danym kluczem
+            result.put(key, value);
         }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Map<Integer, List<String>> map = groupNames("jan", "adam", "piotr", "kasia");
+        System.out.println(map);
     }
 }
